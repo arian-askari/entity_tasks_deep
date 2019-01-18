195de938bbd0e1b69416a0010d3476efa24b58fa
ArianAskari <diyardownload@gmail.com>
2019-01-19 03:19:36 +0330

    add utils for entity retireval with their type and abstract

---
+++ utils/entity_retrieval.py
@@ -0,0 +1,26 @@
+import os, subprocess, json, ast, sys, re, random, csv
+import seaborn as sns
+
+def retrieve_types(query, k=10):  # retrieve top k type for query, with nordlys :)
+    query = query.replace("'", "")
+
+    # query  = 'eminem'
+    cmd = "python3.6 -m nordlys.services.tti -q '" + query + "'"
+
+    prc = str(subprocess.check_output(cmd, shell=True));
+    prc = prc.replace('\n', '').replace('\r', '').replace('\\n', '')
+    prc = prc[1:]
+
+    d = json.loads(prc)
+    d = ast.literal_eval(d)
+
+    top_types = []
+    for result_key, result_detail in d['results'].items():
+        type_ = result_detail['type']
+
+        # type_ = re.findall(r'dbo:(.*?)>', type_)[0]
+        score = result_detail['score']
+        rank = int(result_key)
+        top_types.append((type_, score, rank))
+
+    return top_types
