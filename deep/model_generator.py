adde7822b4f119502c8c63d0c6c691d957cd664d
ArianAskari <diyardownload@gmail.com>
2019-01-31 17:12:08 +0330

    add some detail for how to use this helper

--- deep/model_generator.py
+++ deep/model_generator.py
@@ -1,7 +1,4 @@
-import os, json, random, sys
-import pandas as pd
-
-import os
+import os, sys
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 from keras.layers import *
@@ -9,21 +6,14 @@ from keras.models import *
 from keras import optimizers
 from keras.callbacks import CSVLogger
 
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import KFold
-from keras.wrappers.scikit_learn import KerasClassifier
-from keras.wrappers.scikit_learn import KerasRegressor
-
-
 class Model_Generator():
-    """
-    activation : [relu, relu, linear] means, first and second layers have activation relu, and third(output) layer have linear activation function
-    layers: [1000,1000] means two layers, each layer have 1000 neurons
-    optimizer: default value, adam
-
-    """
-    def __init__(self, layers, activation=[], optimizer="adam", loss = "mse", learning_rate=0.0001,
-                 lambd=0, batch_size=100, epochs=1000, dropout=0.0, csv_log_path = "", verbose = 2):
+    def __init__(self, layers, activation=[], optimizer="adam", loss="mse", learning_rate=0.0001,
+                 lambd=0, batch_size=100, epochs=1000, dropout=0.0, csv_log_path="", verbose=2):
+        """
+            :param activation : [relu, relu, linear] means, first and second layers have activation relu, and third(output) layer have linear activation function
+            :param layers: [1000,1000] means two layers, each layer have 1000 neurons
+            :param optimizer: default value, adam
+        """
 
         self.__layers = layers
         self.__activation = activation
@@ -35,12 +25,13 @@ class Model_Generator():
         self.__epochs = epochs
         self.__dropout = dropout
         self.__csv_log_path = csv_log_path
+        self.__verbose = verbose
         self.__network = None
         self.__history = None
         self.__sess = None
         print("layers =", layers, ", activation =", activation, ", optimizer =", optimizer, ", loss =", loss,
               ", lambda =", lambd, ", learning_rate =", learning_rate, ", batch_size =", batch_size, " ,epochs =", epochs,
-              " ,dropout =", dropout, " ,csv_log_path =", csv_log_path)
+              " ,dropout =", dropout, " ,csv_log_path =", csv_log_path, " ,verbose =", verbose)
 
 
     def fit(self, train_x, train_y, input_dim): #input_dim example: (600,)
@@ -54,17 +45,18 @@ class Model_Generator():
             self.__network.add(Activation(self.__activation[i]))
             self.__network.add(Dropout(self.__dropout))
 
-        adam = None
+
         if (self.__optimizer == "adam"):
             adam = optimizers.Adam(lr=self.__learning_rate)
+            self.__network.compile(optimizer=adam, loss=self.__loss, metrics=["accuracy"])
 
-        self.__network.compile(optimizer=adam, loss=self.__loss, metrics=["accuracy"])
+        self.__network.compile(optimizer=self.__optimizer, loss=self.__loss, metrics=["accuracy"])
 
         if len(self.__csv_log_path)>0:
             csv_logger = CSVLogger(self.__csv_log_path, append=False, separator=',')
-            self.__history = self.__network.fit(train_x, train_y, epochs=self.__epochs, batch_size=self.__batch_size, verbose=2, callbacks=[csv_logger])
+            self.__history = self.__network.fit(train_x, train_y, epochs=self.__epochs, batch_size=self.__batch_size, verbose=self.__verbose, callbacks=[csv_logger])
         else:
-            self.__history = self.__network.fit(train_x, train_y, epochs=self.__epochs, batch_size=self.__batch_size, verbose=2)
+            self.__history = self.__network.fit(train_x, train_y, epochs=self.__epochs, batch_size=self.__batch_size, verbose=self.__verbose)
 
         result = dict()
         result["model"] = self.__network
@@ -96,6 +88,7 @@ class Model_Generator():
 
 
     def predict(self, test_x, test_y=None):
+        """ Performs prediction."""
         result = dict()
 
         output_activation = self.__activation[len(self.__activation)-1]
@@ -115,3 +108,29 @@ class Model_Generator():
 
         return result
 
+
+def example():
+    layers = [100, 100, 2]
+    # activation =  ["relu", "relu", "linear"]
+    activation =  ["relu", "relu", "softmax"]
+
+    X_Train = np.array([ [1, 2, 3], [100, 300, 500] ])
+    # Y_Train = np.array([0, 1])  # regression sample
+    Y_Train = np.array([[1, 0], [0, 1]])  # classification sample
+
+    X_Test = np.array([ [2, 4, 6], [200, 600, 1000] ])
+    # Y_Test = np.array([0, 1])  # regression sample
+    Y_Test = np.array([[1, 0], [0, 1]])  # classification sample
+
+    input_dim = (3,)
+
+    model = Model_Generator(layers=layers, activation=activation, epochs=100, dropout=0.4)
+    result = model.fit(X_Train, Y_Train, input_dim)
+    predict_result = model.predict(X_Test, Y_Test)
+
+    print(result)
+    print("\n\n--------------------------\n\n\n")
+    print(predict_result)
+
+
+#example  #uncomment it for see how to work with this helper :)
