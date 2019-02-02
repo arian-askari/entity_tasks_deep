2aa091f86da89e63456c05f06bd61d5fb4f24f31
ArianAskari <diyardownload@gmail.com>
2019-02-02 11:41:34 +0330

    add nested cross validation, update model generator class

--- deep/model_generator.py
+++ deep/model_generator.py
@@ -8,7 +8,7 @@ from keras.callbacks import CSVLogger
 
 class Model_Generator():
     def __init__(self, layers, activation=[], optimizer="adam", loss="mse", learning_rate=0.0001,
-                 lambd=0, batch_size=100, epochs=1000, dropout=0.0, csv_log_path="", verbose=2):
+                 lambd=0, batch_size=100, epochs=1000, dropout=0.0, verbose=2, category="regression"):
         """
             :param activation : [relu, relu, linear] means, first and second layers have activation relu, and third(output) layer have linear activation function
             :param layers: [1000,1000] means two layers, each layer have 1000 neurons
@@ -24,17 +24,23 @@ class Model_Generator():
         self.__batch_size = batch_size
         self.__epochs = epochs
         self.__dropout = dropout
-        self.__csv_log_path = csv_log_path
         self.__verbose = verbose
+        self.__category = category
+        self.__model_name = self.model_name()
         self.__network = None
         self.__history = None
+        self.__csv_log_path = ""
         self.__sess = None
         print("layers =", layers, ", activation =", activation, ", optimizer =", optimizer, ", loss =", loss,
               ", lambda =", lambd, ", learning_rate =", learning_rate, ", batch_size =", batch_size, " ,epochs =", epochs,
-              " ,dropout =", dropout, " ,csv_log_path =", csv_log_path, " ,verbose =", verbose)
+              " ,dropout =", dropout, " ,verbose =", verbose, " ,category =", category)
 
+    def set_csv_log_path(self, csv_log_path):
+        self.__csv_log_path = csv_log_path
 
     def fit(self, train_x, train_y, input_dim): #input_dim example: (600,)
+        """ Performs training on train_x instances"""
+
         self.__network = Sequential()
         self.__network.add(Dense(self.__layers[0], input_shape=input_dim))
         self.__network.add(Activation(self.__activation[0]))
@@ -52,7 +58,7 @@ class Model_Generator():
 
         self.__network.compile(optimizer=self.__optimizer, loss=self.__loss, metrics=["accuracy"])
 
-        if len(self.__csv_log_path)>0:
+        if len(self.__csv_log_path) > 0:
             csv_logger = CSVLogger(self.__csv_log_path, append=False, separator=',')
             self.__history = self.__network.fit(train_x, train_y, epochs=self.__epochs, batch_size=self.__batch_size, verbose=self.__verbose, callbacks=[csv_logger])
         else:
@@ -60,7 +66,7 @@ class Model_Generator():
 
         result = dict()
         result["model"] = self.__network
-        result["train_loss_mean"] = np.mean(self.__history.history['loss'])
+        result["train_loss_latest"] = self.__history.history['loss'][-1]
         result["train_acc_mean"] = np.mean(self.__history.history['acc'])
 
         result["train_loss"] = self.__history.history['loss']
@@ -72,9 +78,10 @@ class Model_Generator():
         return result
 
     def get_train_loss_mean(self):
+        """ get loss and acc during training."""
         if self.__network is not None:
             result = dict()
-            result["train_loss_mean"] = np.mean(self.__history.history['loss'])
+            result["train_loss_latest"] = self.__history.history['loss'][-1]
             result["train_acc_mean"] = np.mean(self.__history.history['acc'])
 
             result["train_loss"] = self.__history.history['loss']
@@ -104,33 +111,67 @@ class Model_Generator():
             result["predict_prob"] = predict_prob_values
 
         if test_y is not None:
+            test_y = np.array(test_y)
+            print(test_y)
             result["loss_mean"], result["acc_mean"] = self.__network.evaluate(test_x, test_y, verbose=0)
 
         return result
 
+    def model_name(self):
+        """
+        Generate Name for Model from __init__ parameters
+        TODO:: from model summary or from config :)
+        :return: model name
+        """
+        #config = self.__network.get_config()  # TODO:: use this obj for create model name, detailed :)
+        model_name = ""
+        for i in range(len(self.__layers)):
+            model_name += "_L"+ str(i+1) + "(" + str(self.__layers[i]) + ")"
+
+        model_name += "_" + self.__activation[0] +"_drop(" + str(self.__dropout) + ")_opt(" + self.__optimizer + ")"
+        model_name += "_epoch(" + str(self.__epochs) + ")" + "_" + "bsize(" + str(self.__batch_size) + ")"
+        model_name += "_lr(" + str(self.__learning_rate) + ")"
+        model_name += "_lambda(" + str(self.__lambd) + ")"
+        model_name += "_loss(" + str(self.__loss) + ")"
+        model_name += "_" + str(self.__category)
+        return model_name
+
+    def get_model_name(self):
+        return self.__model_name
+
 
 def example():
-    layers = [100, 100, 2]
-    # activation =  ["relu", "relu", "linear"]
-    activation =  ["relu", "relu", "softmax"]
+    layers = [100, 100, 1]  # regression sample
+    # layers = [100, 100, 2]  # classification sample
 
-    X_Train = np.array([ [1, 2, 3], [100, 300, 500] ])
-    # Y_Train = np.array([0, 1])  # regression sample
-    Y_Train = np.array([[1, 0], [0, 1]])  # classification sample
+    activation =  ["relu", "relu", "linear"]
+    # activation =  ["relu", "relu", "softmax"]
 
-    X_Test = np.array([ [2, 4, 6], [200, 600, 1000] ])
-    # Y_Test = np.array([0, 1])  # regression sample
-    Y_Test = np.array([[1, 0], [0, 1]])  # classification sample
+    X_Train = np.array([[1, 2, 3], [100, 300, 500]])
+
+    Y_Train = np.array([0, 1])  # regression sample
+    # Y_Train = np.array([[1, 0], [0, 1]])  # classification sample
+
+    X_Test = np.array([ [2, 4, 6], [200, 600, 1000]])
+
+    Y_Test = np.array([0, 1])  # regression sample
+    # Y_Test = np.array([[1, 0], [0, 1]])  # classification sample
 
     input_dim = (3,)
 
-    model = Model_Generator(layers=layers, activation=activation, epochs=100, dropout=0.4)
+    # model = Model_Generator(layers=layers, activation=activation, epochs=100, dropout=0.4, category="classification")  # classification sample
+    model = Model_Generator(layers=layers, activation=activation, epochs=100, dropout=0.4, category="regression")  # regression sample
+
     result = model.fit(X_Train, Y_Train, input_dim)
     predict_result = model.predict(X_Test, Y_Test)
+
+    print(model.get_model_name())
+    print("\n\n--------------------------\n\n\n")
 
     print(result)
     print("\n\n--------------------------\n\n\n")
     print(predict_result)
 
+    model.get_model_name()
 
-#example  #uncomment it for see how to work with this helper :)
+# example()  #uncomment it for see how to work with this helper :)
---
+++ deep/nested_cross.py
@@ -0,0 +1,143 @@
+import os, json, random, sys
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+from keras.layers import *
+from utils import trec_output as trec
+from utils import file_utils
+from deep import train_set_generator as tsg
+from deep.model_generator import Model_Generator
+
+queries_path = os.path.join("../data", "dbpedia-v1", "queries_type_retrieval.json")
+models_path = os.path.join("../data", "runs", "")
+
+input_name = "input(abstract_e_avg)_"
+input_dim = (600,)
+category = "regression"
+
+def substrac_dicts(dict1, dict2):
+    return dict(set(dict1.items()) - set((dict(dict2)).items()))
+
+epoch_count = 10000
+layers_for_evaluate = [[1000,1000,1], [1000,1000,100,1]]
+activation_for_evaluate = [["relu","relu","linear"],["relu", "relu", "relu", "linear"]]
+dropout_rates = [0.2,0.3,0.4,0.5]
+
+''' test '''
+# epoch_count = 1
+# layers_for_evaluate = [[1, 1], [4, 1],[2, 1]]
+# activation_for_evaluate = [["relu",  "linear"], ["relu", "linear"], ["relu", "linear"]]
+# dropout_rates = [0.1]
+
+def nested_cross_fold_validation():
+    with open(queries_path, 'r') as ff:
+        trec_output_test = ""
+
+        queries_json = json.load(ff)
+
+        queries_dict = dict(queries_json)
+
+        queries_for_select_test_set = dict(queries_dict)
+        k_fold = 5
+
+        # K cross fold validation
+        for i in range(k_fold):
+            global current_fold
+            current_fold = str(i)
+            fold_size = int(len(queries_dict) / k_fold)
+
+            queries_for_test_set = None
+            if (i + 1) == k_fold:
+                queries_for_test_set = list(queries_for_select_test_set.items())
+            else:
+                queries_for_test_set = random.sample(list(queries_for_select_test_set.items()), fold_size)
+
+            queries_for_select_test_set = substrac_dicts(queries_for_select_test_set, queries_for_test_set)
+
+            queries_for_train = substrac_dicts(queries_dict, queries_for_test_set)
+
+            ''' hyper paramter tuning '''
+            models_during_validation = []
+
+            for layers, activation in zip(layers_for_evaluate, activation_for_evaluate):
+                for dropout_rate in dropout_rates:
+                    queries_for_select_validation_set = dict(queries_for_train)
+                    fold_size = int(len(queries_for_train) / (k_fold - 1))
+                    trec_output_validation = ""
+                    model_name = ""
+
+                    for j in range(k_fold - 1):
+                        current_fold = str(j+1)
+
+                        queries_validation_set = None
+                        if (j + 1) == (k_fold - 1):
+                            queries_validation_set = list(queries_for_select_validation_set.items())
+                        else:
+                            queries_validation_set = random.sample(list(queries_for_select_validation_set.items()), fold_size)
+
+                        queries_for_select_validation_set = substrac_dicts(queries_for_select_validation_set, queries_validation_set)
+
+                        queries_train_set = substrac_dicts(queries_for_train, queries_validation_set)
+                        train_X, train_Y, test_X, test_Y_one_hot, q_id_test_list, test_TYPES, test_Y = tsg.get_train_test_data(
+                            queries_train_set, queries_validation_set)
+                        train_Y = np.argmax(train_Y, axis=1)
+
+                        model = Model_Generator(layers=layers, activation=activation, epochs=epoch_count, dropout=dropout_rate,
+                                                category=category, learning_rate=0.0001, loss="mse")  # regression sample
+
+                        model_name = model.get_model_name()
+
+                        log_path = models_path + input_name + "T" + str(i+1) + "_V" + str(j+1) + model_name + ".log"
+
+                        model.set_csv_log_path(log_path)
+
+                        ressult_train = model.fit(train_X, train_Y, input_dim)
+
+                        result_validation = model.predict(test_X, test_Y)
+                        predict_values = result_validation["predict"]
+
+                        trec_output_validation += trec.get_trec_output_regression(q_id_test_list, test_TYPES, test_Y, predict_values)
+
+                        loss_train, acc_train = ressult_train["train_loss_latest"], ressult_train["train_acc_mean"]
+                        loss_validation, acc_validation = result_validation["loss_mean"], result_validation["acc_mean"]
+
+                        models_during_validation.append((model, loss_train,
+                                                         acc_train, loss_validation, acc_validation))
+
+                        #inja mishe yek record baraye config rooye in Ti , V, ba in config ha, ezafe konim dar csv e report :) !
+
+                        print("\n-----------------------------------------------\n\n\n")
+
+                    model_path_run_validation = models_path + input_name + "T" + str(i+1) + "_V(all)_" + model_name + ".run"
+
+                    trec_output_validation = trec_output_validation.rstrip('\n')
+                    file_utils.create_file(model_path_run_validation, trec_output_validation)
+
+
+            ''' Evaluate best model on Test (unseen data :) ) '''
+            _, __, test_X, test_Y_one_hot, q_id_test_list, test_TYPES, test_Y = tsg.get_train_test_data(queries_for_train, queries_for_test_set)
+
+            models_sorted = sorted(models_during_validation, key=lambda x: x[3])  # ascending sort
+
+            best_model, loss_train, acc_train, loss_validation, acc_validation = models_sorted[0]
+            result_test = best_model.predict(test_X, test_Y)
+            loss_test, acc_test = result_test["loss_mean"], result_test["acc_mean"]
+            predict_values = result_test["predict"]
+
+            best_model_name = best_model.get_model_name()
+
+            model_test_fold_run_path = models_path + "T" + str(i+1) + "(bestModel)_" + best_model_name + ".run"
+            trec_output_test += trec.get_trec_output_regression(q_id_test_list, test_TYPES, test_Y, predict_values)
+
+            temp = trec_output_test.rstrip('\n')
+            file_utils.create_file(model_test_fold_run_path, temp)
+
+            print("\n-----------------------------------------------\n")
+
+        model_test_all_run_path = models_path + input_name + "_T(all)" + "_" + category + ".run"
+        trec_output_test = trec_output_test.rstrip('\n')
+
+        file_utils.create_file(model_test_all_run_path, trec_output_test)
+
+
+nested_cross_fold_validation()
