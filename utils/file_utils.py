a46d67819468a738b6f780a98bdf0d1e36609d11
Arian Askari <diyardownload@gmail.com>
2019-02-05 05:48:10 +0330

    upgrade file utils

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,3 +1,6 @@
+import json
+import csv
+
 def create_file(path, data):
     f = open(path, 'w')
     f.write(data)
@@ -8,3 +11,20 @@ def append_file(path, data):
     f = open(path, 'a')
     f.write(data)
     f.close()
+
+def write_file(path, data, force=False):
+    f= None
+
+    if force == False:
+        f = open(path, 'a')
+    else:
+        f = open(path, 'w')
+
+    f.write(data)
+    f.close()
+
+def wirte_json_file(path, data, force=False):
+    if force == False:
+        json.dump(data, fp=open(path, 'a'))
+    else:
+        json.dump(data, fp=open(path, 'w'))
